d6993bf8189ab81d4bb49d9b1d331f9903701dd7
Yazan Tarifi <yazantarifi98@gmail.com>
2020-01-21 04:48:57 +0000

    finish boarding screen

---
+++ app/src/main/java/com/yazan98/culttrip/client/fragment/Test.kt
@@ -0,0 +1,4 @@
+package com.yazan98.culttrip.client.fragment
+
+class Test {
+}
---
+++ app/src/main/java/com/yazan98/culttrip/client/fragment/reg/OnBoardingFragment.kt
@@ -0,0 +1,27 @@
+package com.yazan98.culttrip.client.fragment.reg
+
+import android.view.View
+import com.yazan98.culttrip.client.R
+import io.vortex.android.ui.fragment.VortexBaseFragment
+import kotlinx.android.synthetic.main.fragment_on_boarding.*
+import javax.inject.Inject
+
+class OnBoardingFragment @Inject constructor() : VortexBaseFragment() {
+
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_on_boarding
+    }
+
+    override fun initScreen(view: View) {
+        BoardingIcon?.apply {
+            this.setImageURI("res:/${R.drawable.screen_icon}")
+        }
+
+        ContinueButton?.apply {
+            this.setOnClickListener {
+
+            }
+        }
+    }
+
+}
